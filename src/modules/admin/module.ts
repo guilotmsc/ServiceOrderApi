60b5db0aa3bf1bea181aa8528f3c4da87ed92c2d
tarcisio <guilotmsc@gmail.com>
2022-03-22 20:42:33 -0300

    feat(module): importa order/order-items services e repositories no modulo admin

--- src/modules/admin/module.ts
+++ src/modules/admin/module.ts
@@ -3,17 +3,31 @@ import { CommonModule } from 'modules/common/module';
 import { DatabaseModule } from 'modules/database/module';
 
 import { AuthController } from './controllers/auth';
+import { OrderController } from './controllers/order';
+import { OrderItemController } from './controllers/order-item';
 import { TestController } from './controllers/test';
 import { UserController } from './controllers/user';
 import { RenewTokenMiddleware } from './middlewares/renewToken';
+import { OrderRepository } from './repositories/order';
+import { OrderItemRepository } from './repositories/order-item';
 import { UserRepository } from './repositories/user';
 import { AuthService } from './services/auth';
+import { OrderService } from './services/order';
+import { OrderItemService } from './services/order-item';
 import { UserService } from './services/user';
 
 @Module({
   imports: [HttpModule, CommonModule, DatabaseModule],
-  controllers: [AuthController, UserController, TestController],
-  providers: [AuthService, UserRepository, UserService]
+  controllers: [AuthController, UserController, OrderController, OrderItemController, TestController],
+  providers: [
+    AuthService,
+    UserRepository,
+    UserService,
+    OrderService,
+    OrderRepository,
+    OrderItemService,
+    OrderItemRepository
+  ]
 })
 export class AdminModule implements NestModule {
   public configure(consumer: MiddlewareConsumer) {
